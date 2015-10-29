43c5a54ec47ee3a0b6e76874e0cfe3fc47f8c2e9
=Bindu Laxminarayan <bindu@hexbytes.com>
2015-10-29 13:51:56 -0400

    Added css style for background Highlighted the visited links Highlighting the list on hover over

--- HeadLines/news.js
+++ HeadLines/news.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
 	if( (currentDate - storedDate)>1800000 || (localStorage.getItem("links")==null)){
 		var links=[];
 		var content=[];
-		var searchUrl = 'https://news.google.com/';
+        var searchUrl = 'https://news.google.com/';
 		var x = new XMLHttpRequest();
 		x.open('GET', searchUrl,false);
 		x.send(null);
 		if(x.status==200){
 			var dummyDomEle = document.createElement('div');
-			dummyDomEle.innerHTML = x.responseText;  
-			var orderList = document.createElement('ol');
+			dummyDomEle.innerHTML = x.responseText;
+			var orderList = document.createElement('ul');
 			var titleHeaders = dummyDomEle.getElementsByClassName("esc-lead-article-title");
 			for(i=0;i<titleHeaders.length;i++){
 				var list = document.createElement('li');
@@ -28,15 +28,14 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 			localStorage.setItem("time",new Date());
 			localStorage.setItem("links", JSON.stringify(links));
-			localStorage.setItem("content",JSON.stringify(content));
+			localStorage.setItem("linkContent",JSON.stringify(content));
 			document.getElementById("lastUpdated").innerHTML=currentDate;
 			document.getElementById("newsList").appendChild(orderList);
 		}
 	}else{
-		var orderList = document.createElement('ol');
-		
+		var orderList = document.createElement('ul');
 		var links = JSON.parse(localStorage.getItem("links"));
-		var content = JSON.parse(localStorage.getItem("content"));
+		var content = JSON.parse(localStorage.getItem("linkContent"));
 		for(i=0;i<links.length;i++){
 			var list = document.createElement('li');
 			var link = document.createElement('a');
@@ -46,9 +45,13 @@ document.addEventListener('DOMContentLoaded', function() {
 			link.setAttribute("target","_blank");
 			link.innerHTML=linkTitle;
 			list.appendChild(link);
-			orderList.appendChild(list);			
+			orderList.appendChild(list);
 		}
 		document.getElementById("lastUpdated").innerHTML=new Date(storedDate);
 		document.getElementById("newsList").appendChild(orderList);
 	}
 });
+
+function reload(){
+alert("hi");
+}
